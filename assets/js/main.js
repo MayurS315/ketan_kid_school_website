2db63be11ef880379dadf059c27f952426fee68c
admin <sutarmayur410@gmail.com>
2023-12-17 10:32:45 +0530

    About page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,28 @@ window.onscroll = function() {
         nav.classList.remove("is-sticky");
     }
 }
+
+$(".testimonials-slides").owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    autoplayHoverPause: true,
+    autoplay: true,
+    mouseDrag: true,
+    margin: 30,
+ 
+    responsive: {
+        0:{
+            items:1,
+            nav: false
+          },
+          600:{
+            items:2,
+            nav: false
+          },
+          1000:{
+            items:3,
+            nav: false
+          }
+    }
+});
